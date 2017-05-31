8aeb38e59526af538982fea8b5dd8e5abb6e8eb9
gkrause <gkrause@localhost>
2017-05-31 17:30:20 -0300

    add o projeto 1

---
+++ src/principal/ProxyDemo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author gkrause
+ */
+
+import proxy.AudioIF;
+import proxy.ProxyAudio;
+
+public class ProxyDemo
+  {
+    public static void main(String[] args)
+      {
+        AudioIF meuAudio = new ProxyAudio("meuArquivo.mp3");
+        
+        meuAudio.play();
+          System.out.println();
+          
+          
+          meuAudio.play();
+          System.out.println();
+      }
+  }
